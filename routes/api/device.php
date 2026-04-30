6b93b79ad5d4c9fae02c604f3e136e177b318330
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2026-05-01 03:15:44 +0800

    Update device.php

--- routes/api/device.php
+++ routes/api/device.php
@@ -14,214 +14,184 @@ use Illuminate\Http\Request;
 Route::prefix('/devices')->name('.devices')->group(function(){
     
     //LIST & GET
-    Route::get('/list', [
-        "uses"=>[DeviceAccessController::class, 'list'],
-        "description"=>"List of devices",
-        "is_visible"=>true,
-        "is_allow"=>false
-    ])->name('.list');
+    Route::get('/list', [DeviceAccessController::class, 'list'])
+        ->defaults("_description", "List of devices")
+        ->defaults("_is_visible", true)
+        ->defaults("_is_allow", false)
+        ->name('.list');
 
-    Route::get('list-selection', [
-        "uses"=>[DeviceAccessController::class, 'list_selection'],
-        "description"=>"List selection of device",
-        "is_visible"=>true,
-        "is_allow"=>false
-    ])->name('.list-selection');
+    Route::get('list-selection', [DeviceAccessController::class, 'list_selection'])
+        ->defaults("_description", "List selection of device")
+        ->defaults("_is_visible", true)
+        ->defaults("_is_allow", false)
+        ->name('.list-selection');
 
     //GET
-    Route::get('get', [
-        "uses"=>[DeviceAccessController::class, 'get'],
-        "description"=>"Get a device info",
-        "is_visible"=>true,
-        "is_allow"=>false
-    ])->name('.get');
+    Route::get('get', [DeviceAccessController::class, 'get'])
+        ->defaults("_description", "Get a device info")
+        ->defaults("_is_visible", true)
+        ->defaults("_is_allow", false)
+        ->name('.get');
 
     //ADD
-    Route::post('add', [
-        "uses"=>[DeviceAccessController::class, 'add'],
-        "description"=>"Add a device",
-        "is_visible"=>true,
-        "is_allow"=>false
-    ])->name('.add');
+    Route::post('add', [DeviceAccessController::class, 'add'])
+        ->defaults("_description", "Add a device")
+        ->defaults("_is_visible", true)
+        ->defaults("_is_allow", false)
+        ->name('.add');
 
     //UPDATE
-    Route::post('save', [
-        "uses"=>[DeviceAccessController::class, 'save'],
-        "description"=>"Save a device",
-        "is_visible"=>false,
-        "is_allow"=>false
-    ])->name('.save');
+    Route::post('save', [DeviceAccessController::class, 'save'])
+        ->defaults("_description", "Save a device")
+        ->defaults("_is_visible", false)
+        ->defaults("_is_allow", false)
+        ->name('.save');
 
     //DELETE
-    Route::post('delete', [
-        "uses"=>[DeviceAccessController::class, 'remove'],
-        "description"=>"Remove a device",
-        "is_visible"=>false,
-        "is_allow"=>false
-    ])->name('.remove');
+    Route::post('delete', [DeviceAccessController::class, 'remove'])
+        ->defaults("_description", "Remove a device")
+        ->defaults("_is_visible", false)
+        ->defaults("_is_allow", false)
+        ->name('.remove');
 
-    Route::get('logs', [
-        "uses"=>[DeviceAccessTriggerLogController::class, 'list'],
-        "description"=>"Device trigger logs",
-        "is_visible"=>false,
-        "is_allow"=>true
-    ])->name('.logs');
+    Route::get('logs', [DeviceAccessTriggerLogController::class, 'list'])
+        ->defaults("_description", "Device trigger logs")
+        ->defaults("_is_visible", false)
+        ->defaults("_is_allow", true)
+        ->name('.logs');
 
-    Route::get('dynamic-selection', [
-        "uses"=>[DeviceAccessController::class, 'dynamic_selection'],
-        "description"=>"Table dynamic selection for device trigger",
-        "is_visible"=>true,
-        "is_allow"=>false
-    ])->name('.dynamic-selection');
+    Route::get('dynamic-selection', [DeviceAccessController::class, 'dynamic_selection'])
+        ->defaults("_description", "Table dynamic selection for device trigger")
+        ->defaults("_is_visible", true)
+        ->defaults("_is_allow", false)
+        ->name('.dynamic-selection');
 
-    Route::get('target-table-info', [
-        "uses"=>[DeviceAccessController::class, 'target_table_info'],
-        "description"=>"Target table info for device trigger",
-        "is_visible"=>true,
-        "is_allow"=>false
-    ])->name('.target-table-info');
+    Route::get('target-table-info', [DeviceAccessController::class, 'target_table_info'])
+        ->defaults("_description", "Target table info for device trigger")
+        ->defaults("_is_visible", true)
+        ->defaults("_is_allow", false)
+        ->name('.target-table-info');
 
     Route::prefix('accounts')->name('.accounts')->group(function(){
 
         //LIST
-        Route::get('list-device-triggers', [
-            "uses"=>[DeviceAccountController::class, 'list_device_triggers'],
-            "description"=>"List device triggers for an account",
-            "is_visible"=>true,
-            "is_allow"=>false
-        ])->name('.list-device-triggers');
+        Route::get('list-device-triggers', [DeviceAccountController::class, 'list_device_triggers'])
+            ->defaults("_description", "List device triggers for an account")
+            ->defaults("_is_visible", true)
+            ->defaults("_is_allow", false)
+            ->name('.list-device-triggers');
 
         //GET ONE
-        Route::get('get-one', [
-            "uses"=>[DeviceAccountController::class, 'get_one'],
-            "description"=>"Get a specific device trigger for an account",
-            "is_visible"=>true,
-            "is_allow"=>false
-        ])->name('.get-one');
+        Route::get('get-one', [DeviceAccountController::class, 'get_one'])
+            ->defaults("_description", "Get a specific device trigger for an account")
+            ->defaults("_is_visible", true)
+            ->defaults("_is_allow", false)
+            ->name('.get-one');
 
         //REGISTER
-        Route::post('register-account', [
-            "uses"=>[DeviceAccountController::class, 'register_account'],
-            "description"=>"Implement registration for a device account",
-            "is_visible"=>true,
-            "is_allow"=>false
-        ])->name('.register');
-        Route::post('register-account-preview', [
-            "uses"=>[DeviceAccountController::class, 'register_account_preview'],
-            "description"=>"Preview registration for a device account",
-            "is_visible"=>false,
-            "is_allow"=>true
-        ])->name('.register-preview');
+        Route::post('register-account', [DeviceAccountController::class, 'register_account'])
+            ->defaults("_description", "Implement registration for a device account")
+            ->defaults("_is_visible", true)
+            ->defaults("_is_allow", false)
+            ->name('.register');
+        Route::post('register-account-preview', [DeviceAccountController::class, 'register_account_preview'])
+            ->defaults("_description", "Preview registration for a device account")
+            ->defaults("_is_visible", false)
+            ->defaults("_is_allow", true)
+            ->name('.register-preview');
 
 
         //UPDATE AUTO TRIGGER
-        Route::put('update-auto-trigger', [
-            "uses"=>[DeviceAccountController::class, 'update_auto_trigger'],
-            "description"=>"Update auto trigger for a device account",
-            "is_visible"=>false,
-            "is_allow"=>true
-        ])->name('.update-auto-trigger');
+        Route::put('update-auto-trigger', [DeviceAccountController::class, 'update_auto_trigger'])
+            ->defaults("_description", "Update auto trigger for a device account")
+            ->defaults("_is_visible", false)
+            ->defaults("_is_allow", true)
+            ->name('.update-auto-trigger');
 
         //UPDATE
-        Route::put('update-account', [
-            "uses"=>[DeviceAccountController::class, 'update_account'],
-            "description"=>"Update a device account",
-            "is_visible"=>true,
-            "is_allow"=>false
-        ])->name('.update-account');
-        Route::post('update-account-preview', [
-            "uses"=>[DeviceAccountController::class, 'update_account_preview'],
-            "description"=>"Preview update for a device account",
-            "is_visible"=>false,
-            "is_allow"=>true
-        ])->name('.update-account-preview');
+        Route::put('update-account', [DeviceAccountController::class, 'update_account'])
+            ->defaults("_description", "Update a device account")
+            ->defaults("_is_visible", true)
+            ->defaults("_is_allow", false)
+            ->name('.update-account');
+        Route::post('update-account-preview', [DeviceAccountController::class, 'update_account_preview'])
+            ->defaults("_description", "Preview update for a device account")
+            ->defaults("_is_visible", false)
+            ->defaults("_is_allow", true)
+            ->name('.update-account-preview');
 
         //INACTIVE
-        Route::put('set-inactive', [
-            "uses"=>[DeviceAccountController::class, 'set_inactive_account'],
-            "description"=>"Set a device account as inactive",
-            "is_visible"=>true,
-            "is_allow"=>false
-        ])->name('.set-inactive');
-        Route::post('set-inactive-preview', [
-            "uses"=>[DeviceAccountController::class, 'set_inactive_account_preview'],
-            "description"=>"Preview setting a device account as inactive",
-            "is_visible"=>false,
-            "is_allow"=>true
-        ])->name('.set-inactive-preview');
+        Route::put('set-inactive', [DeviceAccountController::class, 'set_inactive_account'])
+            ->defaults("_description", "Set a device account as inactive")
+            ->defaults("_is_visible", true)
+            ->defaults("_is_allow", false)
+            ->name('.set-inactive');
+        Route::post('set-inactive-preview', [DeviceAccountController::class, 'set_inactive_account_preview'])
+            ->defaults("_description", "Preview setting a device account as inactive")
+            ->defaults("_is_visible", false)
+            ->defaults("_is_allow", true)
+            ->name('.set-inactive-preview');
 
         //ACTIVE
-        Route::put('set-active', [
-            "uses"=>[DeviceAccountController::class, 'set_active_account'],
-            "description"=>"Set a device account as active",
-            "is_visible"=>false,
-            "is_allow"=>true
-        ])->name('.set-active');
-        Route::post('set-active-preview', [
-            "uses"=>[DeviceAccountController::class, 'set_active_account_preview'],
-            "description"=>"Preview setting a device account as active",
-            "is_visible"=>false,
-            "is_allow"=>true
-        ])->name('.set-active-preview');
+        Route::put('set-active', [DeviceAccountController::class, 'set_active_account'])
+            ->defaults("_description", "Set a device account as active")
+            ->defaults("_is_visible", false)
+            ->defaults("_is_allow", true)
+            ->name('.set-active');
+        Route::post('set-active-preview', [DeviceAccountController::class, 'set_active_account_preview'])
+            ->defaults("_description", "Preview setting a device account as active")
+            ->defaults("_is_visible", false)
+            ->defaults("_is_allow", true)
+            ->name('.set-active-preview');
 
         //REMOVE
-        Route::delete('remove', [
-            "uses"=>[DeviceAccountController::class, 'remove_account'],
-            "description"=>"Remove a device account",
-            "is_visible"=>true,
-            "is_allow"=>false
-        ])->name('.remove');
-        Route::post('remove-preview', [
-            "uses"=>[DeviceAccountController::class, 'remove_account_preview'],
-            "description"=>"Preview removal of a device account",
-            "is_visible"=>false,
-            "is_allow"=>true
-        ])->name('.remove-preview');
-
-
-
+        Route::delete('remove', [DeviceAccountController::class, 'remove_account'])
+            ->defaults("_description", "Remove a device account")
+            ->defaults("_is_visible", true)
+            ->defaults("_is_allow", false)
+            ->name('.remove');
+        Route::post('remove-preview', [DeviceAccountController::class, 'remove_account_preview'])
+            ->defaults("_description", "Preview removal of a device account")
+            ->defaults("_is_visible", false)
+            ->defaults("_is_allow", true)
+            ->name('.remove-preview');
     });
 
     //DEVICE TRIIGERS
     Route::prefix('trigger')->name('.trigger')->group(function(){
         
         //LIST
-        Route::get('list', [
-            "uses"=>[DeviceTemplateTriggerController::class, 'list'],
-            "description"=>"Get response from sms sender",
-            "is_visible"=>true,
-            "is_allow"=>false
-        ])->name('.list');
-        Route::get('get-one', [
-            "uses"=>[DeviceTemplateTriggerController::class, 'get_one'],
-            "description"=>"Get a single device template trigger",
-            "is_visible"=>true,
-            "is_allow"=>false
-        ])->name('.get-one');
+        Route::get('list', [DeviceTemplateTriggerController::class, 'list'])
+            ->defaults("_description", "Get response from sms sender")
+            ->defaults("_is_visible", true)
+            ->defaults("_is_allow", false)
+            ->name('.list');
+        Route::get('get-one', [DeviceTemplateTriggerController::class, 'get_one'])
+            ->defaults("_description", "Get a single device template trigger")
+            ->defaults("_is_visible", true)
+            ->defaults("_is_allow", false)
+            ->name('.get-one');
 
         //ADD
-        Route::post('add', [
-            "uses"=>[DeviceTemplateTriggerController::class, 'add'],
-            "description"=>"Add a new device template trigger",
-            "is_visible"=>true,
-            "is_allow"=>false
-        ])->name('.add');
+        Route::post('add', [DeviceTemplateTriggerController::class, 'add'])
+            ->defaults("_description", "Add a new device template trigger")
+            ->defaults("_is_visible", true)
+            ->defaults("_is_allow", false)
+            ->name('.add');
 
         //REMOVE
-        Route::delete('remove/{trigger_device_id}', [
-            "uses"=>[DeviceTemplateTriggerController::class, 'remove'],
-            "description"=>"Remove a device template trigger",
-            "is_visible"=>true,
-            "is_allow"=>false
-        ])->name('.remove');
+        Route::delete('remove/{trigger_device_id}', [DeviceTemplateTriggerController::class, 'remove'])
+            ->defaults("_description", "Remove a device template trigger")
+            ->defaults("_is_visible", true)
+            ->defaults("_is_allow", false)
+            ->name('.remove');
 
         //UPDATE
-        Route::put('update', [
-            "uses"=>[DeviceTemplateTriggerController::class, 'update'],
-            "description"=>"Update a device template trigger",
-            "is_visible"=>true,
-            "is_allow"=>false
-        ])->name('.update');
+        Route::put('update', [DeviceTemplateTriggerController::class, 'update'])
+            ->defaults("_description", "Update a device template trigger")
+            ->defaults("_is_visible", true)
+            ->defaults("_is_allow", false)
+            ->name('.update');
 
 
     });
